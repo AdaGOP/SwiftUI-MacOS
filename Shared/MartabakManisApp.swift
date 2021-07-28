deeead381045ff852a21d0b4c530a4303df50bd4
David <david_gunawan_@hotmail.com>
2021-07-28 15:49:31 +0700

    Initial Commit

---
+++ Shared/MartabakManisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MartabakManisApp.swift
+//  Shared
+//
+//  Created by David Gunawan on 28/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MartabakManisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
