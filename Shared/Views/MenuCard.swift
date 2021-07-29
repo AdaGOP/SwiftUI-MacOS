45728e1391936ece76153dab0cf837e5af5cdaaa
Allicia Viona <alliciawn@gmail.com>
2021-07-29 15:50:03 +0700

    Update Card View

--- Shared/Views/MenuCard.swift
+++ Shared/Views/MenuCard.swift
@@ -14,26 +14,29 @@ struct MenuCard: View {
     
     @ObservedObject var imageCacher = ImageCacher()
     
-    init(menu: MenuRecord) {
-        self.menu = menu
-    }
-    
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             Rectangle()
                 .fill(Color.gray.opacity(0.3))
                 .cornerRadius(8)
                 .shadow(radius: 4)
-            HStack {
+            VStack {
                 if self.imageCacher.image != nil {
                     Image(nsImage: self.imageCacher.image!)
                         .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 204, height: 230)
+                        .cornerRadius(8)
+                        
                 }
-                    
-                Text(menu.fields.nama)
-                    .multilineTextAlignment(.center)
+                
+                VStack {
+                    Text("Rating " + String(menu.fields.rating)).multilineTextAlignment(.center)
+                    Text(menu.fields.nama)
+                        .multilineTextAlignment(.center)
+                    Text("Rp. " + String(menu.fields.harga.formattedWithSeparator)).multilineTextAlignment(.center)
+                }
+                
             }
         }
         .onAppear {
@@ -47,4 +50,17 @@ struct MenuCard_Previews: PreviewProvider {
     static var previews: some View {
         MenuCard(menu: MenuRecord(id: "", fields: Menu(nama: "Martabak Mantap", deskripsi: "Enak Banget", harga: 70000, gambar: "https://dl.airtable.com/.attachments/a1f1b11a4e940823217a51b3fb1d0740/eb5bc690/monika-grabkowska-89HtiQoRgPc-unsplash.jpeg", rating: 4.7), createdTime: ""))
     }
+}
+
+extension Formatter {
+    static let withSeparator: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = "."
+        return formatter
+    }()
+}
+
+extension Numeric {
+    var formattedWithSeparator: String { Formatter.withSeparator.string(for: self) ?? "" }
 }
